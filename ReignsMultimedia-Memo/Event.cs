1e62839ebc4444ac018d79bd2d7e9d2614083091
Armando Meza Leyva <armezaleyva@gmail.com>
2018-11-24 18:49:14 -0700

    Clases de Eventos creados

---
+++ ReignsMultimedia-Memo/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReignsMultimedia_Memo {
+    class Event {
+        // TO DO: Personaje asociado con el evento
+        string Text { get; set; }
+
+        string RightReactionText { get; set; }
+        List<int> RightReactionEffect { get; set; }
+        Event RightReactionSequence { get; set; } 
+
+        string LeftReaction { get; set; }
+        List<int> LeftReactionEffect { get; set; }
+        Event LeftReactionSequence { get; set; }
+    }
+}
